655d31dd271adcab17af9d943d944e79d08edcec
Ruthyanne Priscila <161310200+ruthyinfom@users.noreply.github.com>
2026-08-25 11:56:40 -0300

    Add routes for LojaFront component and redirect

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LojaFront } from './loja-front/loja-front';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "produtos", component: LojaFront }, 
+    { path: "", redirectTo: "/produtos", pathMatch: "full"}
+];
