27d11004410f7d0e25104bffb852c382b27a1bec
Ruthyanne Priscila <161310200+ruthyinfom@users.noreply.github.com>
2026-08-25 15:53:33 -0300

    ts
    Refactor ProdutoComponent to use constructor injection and add logging.

--- frontend/src/app/produto-component/produto-component.ts
+++ frontend/src/app/produto-component/produto-component.ts
@@ -1,23 +1,44 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProdutoService } from '../produto-service';
 import { Produto } from '../produto';
 
 @Component({
   selector: 'app-produto-component',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './produto-component.html',
   styleUrl: './produto-component.css'
 })
-export class ProdutoComponent {
+export class ProdutoComponent implements OnInit {
 
   produtos: Produto[] = [];
 
-  readonly #produtoService = inject(ProdutoService);
+  constructor(
+    private produtoService: ProdutoService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
-  ngOnInit() {
-    this.#produtoService.obterTodos().subscribe({
-      next: (produtos) => {
-        this.produtos = produtos;
+  ngOnInit(): void {
+
+    console.log('ProdutoComponent foi carregado!');
+
+    this.produtoService.obterProdutos().subscribe({
+      next: (dados) => {
+
+        console.log('PRODUTOS RECEBIDOS:', dados);
+        console.log('QUANTIDADE:', dados.length);
+        console.log('TIPO:', typeof dados);
+
+        this.produtos = dados;
+
+        this.cdr.detectChanges();
+      },
+
+      error: (erro) => {
+        console.error('ERRO AO BUSCAR PRODUTOS:', erro);
       }
     });
+
   }
 }
